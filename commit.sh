e5106e3733e7d949291403de2c410cee3bd129c4
Main User <bornelinux@VULCAN>
2024-10-29 03:25:57 +0300

    number 3

--- commit.sh
+++ commit.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Function to check for commits on GitHub
 check_commits() {
-    # Derive the repository name from the current directory name
-    REPO_NAME=$(basename "$PWD")  # Gets the name of the current directory
+    REPO_NAME=$(basename "$PWD")
     LATEST_COMMIT=$(curl -s "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/commits" | jq -r '.[0].sha')
-    
-    # Get the latest local commit hash
     LOCAL_COMMIT=$(git rev-parse HEAD)
 
     if [ "$LATEST_COMMIT" == "$LOCAL_COMMIT" ]; then
@@ -16,7 +12,6 @@ check_commits() {
     fi
 }
 
-# Function to commit local changes
 commit_changes() {
     echo "~Commiting Files..."
     git add .
@@ -24,29 +19,19 @@ commit_changes() {
     git push origin main
 }
 
-# Start of the script
 echo "~Calling Borne Raptor..."
 echo "~Borne Raptor Version1.1"
 
-# Prompt for GitHub username
 read -p "Enter your GitHub username: " GITHUB_USERNAME
 
-# Check if there are uncommitted changes
 if ! git diff-index --quiet HEAD --; then
-    # Inform the user that there are changes to commit
     echo "Changes detected in the repository."
-
-    # Enter your commit message
     read -p "Enter your commit message: " commit_message
     commit_changes
-
-    # Check for commits on GitHub after committing
     check_commits
 
-    # Get the names of committed files
     COMMITTED_FILES=$(git diff --name-only)
 
-    # Display success message and ASCII art
     cat << "EOF"
                                                      ___._
                                                    .'  <0>'-.._
@@ -70,9 +55,7 @@ __________                                     | : '. |
                       'c=,
 EOF
 
-    # Display the commit message and file details on new lines
     echo -e "File [$COMMITTED_FILES] committed to Repository [$REPO_NAME] at [$GITHUB_USERNAME].\n"
 else
-    # No changes detected
     echo "No changes made in the repository."
 fi
