c05253a9ee4e23e545ee37e9ecea91c90251dd86
Main User <bornelinux@VULCAN>
2024-10-30 22:03:17 +0300

    Final Commit

--- commit.sh
+++ commit.sh
@@ -80,7 +80,6 @@ if ! cd "$TARGET_DIR"; then
     exit 1
 fi
 
-# Pull checker added
 if ! git pull origin main --no-rebase; then
     echo "ERROR: Failed to pull latest changes. Please resolve any conflicts."
     exit 1
